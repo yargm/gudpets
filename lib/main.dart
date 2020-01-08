5697120730002e065f0d5eaf9fc85bfa998ca30a
yargm <yargm2304@gmail.com>
2020-01-08 10:54:30 -0600

    Agregué algunas rutas en el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:adoption_app/adoption.dart';
 import 'package:flutter/material.dart';
 import 'home.dart';
+import 'registrousuario.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,7 +11,8 @@ class MyApp extends StatelessWidget {
       home: HomePage(),
       routes: {
         '/home': (BuildContext context) => HomePage(),
-        // '/adoption': (BuildContext context) => AdoptionPage(),
+        '/adoption': (BuildContext context) => Adoption(),
+        '/registrousuario': (BuildContext context) => RegistroUsuario(),
       },
     );
   }
