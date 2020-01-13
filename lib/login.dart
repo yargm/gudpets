774cfc8bc893663232fc7a2c42f7fc3dd64bd72f
chame10 <chamesito10@gmail.com>
2020-01-13 12:41:38 -0600

    añadí la pagina de loogin no era mi branch

---
+++ lib/login.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String error = '';
+
+  Map<String, dynamic> loginMap = {'user': null, 'password': null};
+
+  @override
+Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.amber[100],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.all(4.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 75.0,
+                  ),
+                  Text(
+                    'Iniciar Sesión',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black26),
+                  ),
+                  SizedBox(
+                    height: 75.0,
+                  ),
+                  Card(
+                    elevation: 9.0,
+                    shape: ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.circular(100.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(18.0, 15.0, 18.0, 15.0),
+                              labelText: 'Usuario',
+                              prefixIcon: Icon(Icons.account_circle),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                              labelText: 'Contraseña',
+                              prefixIcon: Icon(Icons.lock),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40.0,
+                          ),
+                          RaisedButton(
+                            onPressed: () => null,
+                            textColor: Colors.black,
+                            elevation: 9.0,
+                            highlightElevation: 6.0,
+                            child: Text(
+                              "Login",
+                              textAlign: TextAlign.center,
+                            ),
+                            shape: BeveledRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
