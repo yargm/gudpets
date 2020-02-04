67f4e7d8ce25bfa7dca5750cd786dd9c1c145d85
chame10 <chamesito10@gmail.com>
2020-02-04 09:41:46 -0600

    adding searchDelegate

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -4,3 +4,4 @@ export 'controlador.dart';
 export 'package:provider/provider.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
 export 'sign_in.dart';
+export 'customSearchDelegate.dart';
